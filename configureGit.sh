ed485a16f7bd6caf538effbaed44877d559ea931
Luke Horowitz <lhorowit@calpoly.edu>
2014-04-03 13:07:02 -0700

    added documentation

--- configureGit.sh
+++ configureGit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+# This script configures Git whenever you start working on multiple machines
+# Run it anytime u decide to work with Git on a new machine.
 git config --global user.name "Luke Horowitz"
 git config --global user.email "lhorowit@calpoly.edu"
 git config --global color.ui "auto"
