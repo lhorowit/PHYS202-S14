593c2fb7b2456aef33ccaf9366e8753f64482045
Luke Horowitz <lhorowit@calpoly.edu>
2014-04-03 12:55:57 -0700

    adding config and pledge

---
+++ configureGit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.name "Luke Horowitz"
+git config --global user.email "lhorowit@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
+ 
